8d273215baef70c8246e3398654829aac11cdced
Kevin Gueders <kevin.gueders@gmail.com>
2013-03-17 18:40:12 +0100

    suppression de fichiers

---
+++ integrationTFE/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="./css/style.css" media="all">
+	<title>AppliTFE</title>
+	<!--JTouch.js et Aptana-->
+</head>
+<body>
+	<?php for($i=0;$i<2;$i++){?>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla molestie leo eget laoreet. Nam sodales convallis luctus. Morbi sed mi quis leo varius dignissim. Nam sit amet quam ligula, sit amet pretium elit. Phasellus vestibulum pellentesque est. Curabitur quis neque quam. Quisque risus diam, aliquet eu feugiat ut, volutpat et libero. In ullamcorper imperdiet posuere. In lorem metus, consectetur ut consectetur accumsan, convallis mollis neque. Proin pellentesque massa sed odio mollis sagittis. Fusce iaculis est in tortor malesuada sagittis commodo velit fermentum. Donec auctor lacus a turpis faucibus feugiat. Aenean eget luctus arcu. Nunc euismod, risus id dapibus tempor, leo magna blandit lorem, a convallis nibh magna ac mauris. </p>
+	<?php }; ?>
+</body>
+</html>
